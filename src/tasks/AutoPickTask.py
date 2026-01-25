7a1dd92616cd1c4d60dbf9092c4a9b0bcacd3995
ok-oldking <154002444+ok-oldking@users.noreply.github.com>
2026-01-25 13:39:56 +0800

    增加自动拾取白名单

--- src/tasks/AutoPickTask.py
+++ src/tasks/AutoPickTask.py
@@ -20,7 +20,7 @@ class AutoPickTask(BaseEfTask, TriggerTask):
         self.default_config = {'_enabled': True}
         self.last_box_name = None
         self.last_pick_time = 0
-        self.white_list = {'采集', '萤壳虫', '打开'}
+        self.white_list = {'采集', '萤壳虫', '打开', '荞花'}
 
     def run(self):
         if self.in_world():
